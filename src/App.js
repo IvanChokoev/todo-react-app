d348623d5db0903ff6b2042876843058febac296
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-04-06 15:00:45 +0300

    initial comments

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 const App = () => {
+
+    // Initialize todos as an empty array using useState
   const [todos, setTodos] = useState([]);
 
+    // Handler function to add a new todo
   const handleTodoAdd = newTodo => {
     setTodos([...todos, newTodo]);
   };
 
+    // Handler function to delete a todo
   const handleTodoDelete = todoId => {
     setTodos(todos => todos.filter(todo => todo.id !== todoId));
   };
 
+    // Handler function to complete a todo
   const handleTodoComplete = todoId => {
     setTodos(todos =>
       todos.map(todo =>
@@ -21,6 +26,7 @@ const App = () => {
     );
   };
 
+    // Render the App component with TodoForm and TodoList components
   return (
     <div className="App">
       <h1>Todo List</h1>
